ce19e4e57e96d99078f08f14394f9aeb87d637a8
Alan_Caro <alancaro2@gmail.com>
2018-05-27 13:22:16 -0400

    Pirmer Layout

---
+++ layouts/app.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+
+app.use("/public",express.static('public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
+
+app.set("view engine", "jade");
+
+app.get("/",function(req,res){
+res.render("index");
+});
+
+app.get("/login",function(req,res){	
+res.render("login");
+});
+
+app.post("/users",function(req,res){
+console.log("Contraseña:" + req.body.password);
+	console.log("Email:" + req.body.email);
+	res.send("recibimos tus datos");
+});
+
+app.listen(8080);
